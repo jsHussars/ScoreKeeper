69af972ba6c7cae6888c32e0cd7d7d16dbf601e5
Jan <45399292+Yah0@users.noreply.github.com>
2019-01-06 14:47:36 +0100

    Update score.js

--- score.js
+++ score.js
@@ -2,8 +2,11 @@
 
 var p1Button = document.querySelector("#p1")
 var p2Button = document.getElementById("p2")
+var resetButton = document.getElementById("reset");
 var p1Display = document.querySelector("#p1Display")
 var p2Display = document.querySelector("#p2Display")
+var numInput = document.querySelector("input");
+var winningScoreDisplay = document.querySelector("p span");
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
@@ -27,4 +30,24 @@ p2Button.addEventListener("click", function(){
 	}
 	p2Display.textContent = p2Score;
 	}
+});
+
+resetButton.addEventListener("click", function(){
+	reset();
+});
+
+function reset(){
+	p1Score = 0;
+	p2Score = 0;
+	p1Display.textContent = 0;
+	p2Display.textContent = 0;
+	p1Display.classList.remove("winner");
+	p2Display.classList.remove("winner");
+	gameOver = false;
+};
+
+numInput.addEventListener("change", function(){
+	winningScoreDisplay.textContent = numInput.value;
+	winningScore = Number(numInput.value);
+	reset();
 });
